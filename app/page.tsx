1984d165b944d63fe64ff19e7b8f8e8968a99a9f
lokesh <lokeshkhati005@gmail.com>
2024-01-24 14:09:40 +0530

    add image config

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useGetUsersQuery } from "@/redux/services/userApi";
 import { decrement, increment, reset } from "@/redux/features/counterSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import Image from "next/image";
 
 export default function Home() {
   const count = useAppSelector((state) => state.counterReducer.value);
@@ -41,10 +42,12 @@ export default function Home() {
               key={user.id}
               style={{ border: "1px solid #ccc", textAlign: "center" }}
             >
-              <img
+              <Image
                 src={`https://robohash.org/${user.id}?set=set2&size=180x180`}
                 alt={user.name}
                 style={{ height: 180, width: 180 }}
+                width={400}
+                height={400}
               />
               <h3>{user.name}</h3>
             </div>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['robohash.org']
+    }
+}
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer(nextConfig)
